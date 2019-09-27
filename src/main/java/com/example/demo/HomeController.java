3370f3350a3d0194a8cbafd7d4a0eab545a9b7b8
Bezaa25 <sirak.bezawit@gmail.com>
2019-09-27 02:15:48 -0400

    added wlb_tips

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -7,8 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
     public String homePage(){return "homePage";}
+    //-----------Content--------------
+    @RequestMapping("/wlb_tips")
+    public String WLB(){return"wlb_tips";}
+
+    //------------end-------------------
     @RequestMapping("/bootcamp")
+
     public String bootCamp(){return "bootcamp";}
     @RequestMapping("/profile")
     public String profile(){return "profile";}
+
 }
